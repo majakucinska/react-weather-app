c07a7a91912910bbb4475cadf49884c09ed3ce4a
Maja Kucinska <maja.kucinska@gmail.com>
2023-07-22 17:46:22 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import './App.css';
 export default function App() {
     return (
       <div className="App">
-        <h1>Weather app</h1>
-        <footer>
-          This project was coded by Maja Kucinska-Kijo and is {""}
-        <a href="https://github.com/majakucinska/react-weather-app" target="_blank">open-sourced on GitHub</a>
-        </footer>
+        <div className="container">
+          <h1>Weather app</h1>
+          <footer>
+            This project was coded by Maja Kucinska-Kijo and is {""}
+          <a href="https://github.com/majakucinska/react-weather-app" target="_blank">open-sourced on GitHub</a>
+          </footer>
+        </div>
       </div>
     );
   }
