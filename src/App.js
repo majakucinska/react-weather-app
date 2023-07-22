af5509a0adb8332adcdcda0b97ae4a32dd4b5134
Maja Kucinska <maja.kucinska@gmail.com>
2023-07-22 16:58:48 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import './App.css';
 export default function App() {
     return (
       <div className="App">
-        Weather app
+        <h1>Weather app</h1>
+        <footer>
+          This project was coded by Maja Kucinska-Kijo and is {""}
+        <a href="https://github.com/majakucinska/react-weather-app" target="_blank">open-sourced on GitHub</a>
+        </footer>
       </div>
     );
   }
