373ec9285d5521ddd4961959b8e08e5e9023706a
Maja Kucinska <maja.kucinska@gmail.com>
2023-08-07 12:52:43 +0200

    Added API integration
    axios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
     return (
       <div className="App">
         <div className="container">
-          <Weather />
+          <Weather defaultCity="New York" />
           <footer>
             This project was coded by Maja Kucinska-Kijo and is {""}
           <a href="https://github.com/majakucinska/react-weather-app" target="_blank">open-sourced on GitHub</a>
