e79a7a63a7116555021fa66dafb9c83092543d09
Maja Kucinska <maja.kucinska@gmail.com>
2023-08-10 13:55:32 +0200

    Added useEffect to update Forecast
    Forecast is updated every time we search for another city (change coordinates)

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -11,6 +11,10 @@ export default function WeatherForecast(props){
         setForecast(response.data.daily);
         setLoaded(true);
     }
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
     
     if(loaded){
         return(
